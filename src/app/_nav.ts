3142a559f0747d0668e4f89d934b1730ee6e328e
anand <anandgp935@gmail.com>
2020-07-03 00:22:28 +0530

    initial commit
    Signed-off-by: anand <anandgp935@gmail.com>

---
+++ src/app/_nav.ts
@@ -0,0 +1,74 @@
+import { INavData } from '@coreui/angular';
+
+export const navItems: INavData[] = [
+  {
+    name: 'Dashboard',
+    url: '/dashboard',
+    icon: 'fa fa-home',
+    // badge: {
+    //   variant: 'info',
+    //   text: 'NEW'
+    // }
+  },
+  {
+    name: 'Activity',
+    url: '/500',
+    icon: 'fa fa-area-chart',
+  },
+  {
+    name: 'Documents',
+    url: '/500',
+    icon: 'fa fa-file',
+  },
+  {
+    name: 'Finances',
+    url: '/500',
+    icon: 'fa fa-dollar',
+  },
+  {
+    name: 'Inventory',
+    url: '/500',
+    icon: 'fa fa-desktop',
+  },
+  {
+    name: 'Items List',
+    url: '/500',
+    icon: 'fa fa-database',
+  },
+  {
+    name: 'Customers',
+    url: '/500',
+    icon: 'fa fa-users',
+  },
+  {
+    name: 'Marketing',
+    url: '/500',
+    icon: 'fa fa-briefcase',
+  },
+  {
+    name: 'Notification Center',
+    url: '/500',
+    icon: 'fa fa-envelope',
+  },
+  {
+    name: 'Staff',
+    url: '/500',
+    icon: 'fa fa-id-card',
+  },
+  {
+    name: 'Reports',
+    url: '/500',
+    icon: 'fa fa-file-text',
+  },
+  {
+    name: 'Profile',
+    url: '/500',
+    icon: 'fa fa-user',
+  },
+  {
+    name: 'LogOut',
+    url: '/500',
+    icon: 'fa fa-sign-out',
+  },
+
+];
